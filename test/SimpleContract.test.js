e948e7a2406ceb2de7aa125f0b4242e09011d749
Gwen <161306951+Gwendolynt123@users.noreply.github.com>
2024-03-08 20:11:28 +0800

    test

--- test/SimpleContract.test.js
+++ test/SimpleContract.test.js
@@ -64,6 +64,76 @@ describe("SimpleContract", function () {
     });
   });
 
+  describe("Increment Value", function () {
+    it("Should allow owner to increment value", async function () {
+      await simpleContract.setValue(10);
+      
+      await expect(simpleContract.incrementValue())
+        .to.emit(simpleContract, "ValueIncremented")
+        .withArgs(11);
+      
+      expect(await simpleContract.getValue()).to.equal(11);
+    });
+
+    it("Should not allow non-owner to increment value", async function () {
+      await expect(
+        simpleContract.connect(otherAccount).incrementValue()
+      ).to.be.revertedWith("Only owner can increment value");
+    });
+
+    it("Should increment value multiple times", async function () {
+      expect(await simpleContract.getValue()).to.equal(0);
+
+      await simpleContract.incrementValue();
+      expect(await simpleContract.getValue()).to.equal(1);
+
+      await simpleContract.incrementValue();
+      expect(await simpleContract.getValue()).to.equal(2);
+
+      await simpleContract.incrementValue();
+      expect(await simpleContract.getValue()).to.equal(3);
+    });
+  });
+
+  describe("Transfer Ownership", function () {
+    it("Should allow owner to transfer ownership", async function () {
+      await expect(simpleContract.transferOwnership(otherAccount.address))
+        .to.emit(simpleContract, "OwnershipTransferred")
+        .withArgs(owner.address, otherAccount.address);
+      
+      expect(await simpleContract.owner()).to.equal(otherAccount.address);
+    });
+
+    it("Should not allow non-owner to transfer ownership", async function () {
+      await expect(
+        simpleContract.connect(otherAccount).transferOwnership(otherAccount.address)
+      ).to.be.revertedWith("Only owner can transfer ownership");
+    });
+
+    it("Should not allow transfer to zero address", async function () {
+      await expect(
+        simpleContract.transferOwnership("0x0000000000000000000000000000000000000000")
+      ).to.be.revertedWith("New owner cannot be zero address");
+    });
+
+    it("Should update owner privileges after transfer", async function () {
+      // Transfer ownership
+      await simpleContract.transferOwnership(otherAccount.address);
+      
+      // Original owner should not be able to set value
+      await expect(
+        simpleContract.setValue(100)
+      ).to.be.revertedWith("Only owner can set value");
+      
+      // New owner should be able to set value
+      await expect(simpleContract.connect(otherAccount).setValue(100))
+        .to.emit(simpleContract, "ValueUpdated")
+        .withArgs(100);
+      
+      expect(await simpleContract.getValue()).to.equal(100);
+    });
+  });
+
   describe("View Functions", function () {
     it("Should return correct value via getValue function", async function () {
       await simpleContract.setValue(999);
